86bae73a673bfc77165bc2de52803927f0c6e0f0
aidskiy <49568146+aidskiy@users.noreply.github.com>
2022-03-05 15:12:35 -0500

    part 2
    superhero

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by ApplePC on 3/5/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
